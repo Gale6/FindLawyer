ee2e378275a41b4ae6291d5c06a70c1d2fa4644e
18xxperson <46502267+18xxperson@users.noreply.github.com>
2022-03-31 15:26:03 -0700

    getting files to compile

--- app/src/main/java/com/revature/findlawyer/Screens.kt
+++ app/src/main/java/com/revature/findlawyer/Screens.kt
@@ -7,7 +7,9 @@ sealed class DrawerScreens (val title: String,val icon:Int, val route:String){
     object MainUser:DrawerScreens("Lawyer_LogIn",R.drawable.ic_baseline_keyboard_arrow_right_24,"user_login_register")
     object LawyerLogin:DrawerScreens("Lawyer LogIn",R.drawable.ic_baseline_keyboard_arrow_right_24,"lawyer_login")
     object UserLogIn:DrawerScreens("User LogIn", R.drawable.ic_baseline_keyboard_arrow_right_24,"user_login")
-
+    object UserRegister:DrawerScreens("User Register",R.drawable.ic_baseline_keyboard_arrow_right_24,"user_register")
+    object LawyerRegister:DrawerScreens("Lawyer Register",R.drawable.ic_baseline_keyboard_arrow_right_24,"lawyer_register")
+    object LawyerSearch:DrawerScreens("Lawyer Search",R.drawable.ic_baseline_keyboard_arrow_right_24,"lawyer_search")
 
 
 }
