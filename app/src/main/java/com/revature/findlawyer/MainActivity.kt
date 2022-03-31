3855b6611d76d397def6f1a9db97fd380a7b5900
RunxiangHuang <gale6233@gmail.com>
2022-03-31 18:14:25 -0400

    commit before merge

--- app/src/main/java/com/revature/findlawyer/MainActivity.kt
+++ app/src/main/java/com/revature/findlawyer/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.revature.findlawyer
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.revature.findlawyer.ui.Screen_CurrentAppointmentScreen
-import com.revature.findlawyer.ui.Screen_ScheduleNewAppointment
-
 import com.revature.findlawyer.ui.theme.FindLawyerTheme
-import com.revature.findlawyer.ui.theme.SimpleAlertDialog
-import com.revature.findlawyer.ui.theme.test
 import com.revature.findlawyer.viewmodel.FetchAppointmentViewModel
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +15,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             FindLawyerTheme {
 
-                test()
 
                 //SimpleAlertDialog()
                 //Screen_ScheduleNewAppointment()
