2745e085cd7a10a5e382fb7dbd6a0a7fa96dc6e1
RunxiangHuang <gale6233@gmail.com>
2022-03-30 22:57:57 -0400

    test commit

--- app/src/main/java/com/revature/findlawyer/ui/Screen_ScheduleNewAppointment.kt
+++ app/src/main/java/com/revature/findlawyer/ui/Screen_ScheduleNewAppointment.kt
@@ -1,75 +1,63 @@
 package com.revature.findlawyer.ui
 
-import android.app.DatePickerDialog
-import android.widget.DatePicker
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import android.os.Build
+import android.util.Log
+import android.widget.NumberPicker
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.*
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Screen_ScheduleNewAppointment() {
 
-    Scaffold(modifier = Modifier.fillMaxSize()) {
+    val current = LocalDateTime.now()
+    Log.d("hih",current.toString())
 
-        val context = LocalContext.current
+    val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm")
 
-        val year: Int
-        val month: Int
-        val day: Int
+    var result = current.format(formatter)
 
-        val calendar = Calendar.getInstance()
+    Log.d("hih",result)
 
-        year = calendar.get(Calendar.YEAR)
-        month = calendar.get(Calendar.MONTH)
-        day = calendar.get(Calendar.DAY_OF_MONTH)
+    var res = LocalDate.parse(result,formatter)
 
-        calendar.time = Date()
+    Log.d("hih",res.toString())
 
-        val date = remember { mutableStateOf("") }
-
-        val mDatePickerDialog = DatePickerDialog(
-            context,
-            { _: DatePicker, year: Int, month: Int, day: Int ->
-                date.value = "$day/${month + 1}/$year"
-            }, year, month, day
-        )
-
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-            // Creating a button that on
-            // click displays/shows the DatePickerDialog
-            Button(onClick = {
-                mDatePickerDialog.show()
-            }, colors = ButtonDefaults.buttonColors(backgroundColor = Color(0XFF0F9D58))) {
-                Text(text = "Open Date Picker", color = Color.White)
-            }
-
-            // Adding a space of 100dp height
-            Spacer(modifier = Modifier.size(100.dp))
-
-            // Dispaying the mDate value in the Text
-            Text(
-                text = "Selected Date: ${date.value}",
-                fontSize = 30.sp,
-                textAlign = TextAlign.Center
+    val state = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        scaffoldState = state,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "AppBar") },
+                navigationIcon = {
+                    Icon(Icons.Rounded.Menu, contentDescription = "Localized description", modifier = Modifier.clickable {
+                        scope.launch {
+                            state.drawerState.open()
+                        }
+                    })
+                }
             )
+        },
+        drawerContent = {
+            Text(text = "Drawer")
         }
+    ) {
+        // Scaffold body
     }
+
+
+
 }
