18fb5a52f21889fa21fb159026995d18e086c000
Lyle <lylelopez03@gmail.com>
2022-04-01 16:47:31 -0700

    Header Component if needed

--- app/src/main/java/com/revature/findlawyer/ui/Components.kt
+++ app/src/main/java/com/revature/findlawyer/ui/Components.kt
@@ -27,10 +27,13 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.heading
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.revature.findlawyer.R
 import com.revature.findlawyer.Screens
@@ -138,5 +141,32 @@ fun LogoMain(){
                 .offset(x = 61.dp, y = 50.dp)
         )
     }
+}
+
+
+//////////////////////////////  HEADER ////////////////////////////////////
+
+@Preview
+@Composable
+fun headerPrieview(){
+    Header(text = "Hello Test")
+}
+
+@Composable
+fun Header(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(fontFamily = Cormorantgaramond,
+        fontWeight = FontWeight.Bold,
+        fontSize = 25.sp,
+        text = text,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.LightGray)
+            .semantics { heading() }
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .offset(130.dp)
+    )
 }
 
