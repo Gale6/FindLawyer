f6dbe6597d79290aeaf4d73a1947d57286c21684
tatebl <brandon.tate15@gmail.com>
2022-04-03 13:34:10 -0400

    Added LawyerSearch and PostReview to screens.kt

--- app/src/main/java/com/revature/findlawyer/Screens.kt
+++ app/src/main/java/com/revature/findlawyer/Screens.kt
@@ -25,6 +25,9 @@ sealed class DrawerScreens (val title: String,val icon:ImageVector,val route:Str
     object Screen_Histories:DrawerScreens("Screen_Histories",Icons.Default.ShoppingCart,"Histories")
     object Screen_CurrentAppointments:DrawerScreens("CurrentAppointments",Icons.Default.ShoppingCart,"CurrentAppointments")
     object Screen_ScheduleNewAppointment:DrawerScreens("ScheduleNewAppointment",Icons.Default.ShoppingCart,"ScheduleNewAppointment")
+    object Screen_LawyerSearch:DrawerScreens("Lawyer Search",Icons.Default.ShoppingCart,"LawyerSearch")
+    object Screen_PostReview:DrawerScreens("Post Review",Icons.Default.ShoppingCart,"PostReview")
+
 
 
 
